b2be67ee4b9fbe24e659c4325ef6a505929732b5
aradzikowska12 <aradzikowska12@gmail.com>
2019-07-04 12:16:48 +0200

    23.3 commit

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.domain;
+
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public Mail(String mailTo, String subject, String message, String toCc) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+        this.toCc = toCc;
+    }
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToCc() {
+        return toCc;
+    }
+}
